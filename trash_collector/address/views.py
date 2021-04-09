d95d9ed72d83b0ab706ee00fc93af73a78f4d729
unknown <ssgsj.carranza@gmail.com>
2021-04-09 15:04:40 -0500

    added deault values to the last six fields under customer, and migrated them to a table

--- trash_collector/address/views.py
+++ trash_collector/address/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+# for test purposes
+def index(request):
+    return HttpResponse("Hi")
--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 
 # Create your models here.
@@ -10,12 +11,12 @@ class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', default=1, on_delete=models.CASCADE)
     pickup_date = models.CharField(max_length=50)
-    # one_time_pick_up = models.DateField()
-    # suspend_start_date = models.DateField()
-    # suspend_end_date = models.DateField()
-    # current_bill = models.FloatField()
-    # pick_up_charge_amount = models.FloatField()
-    # pick_up_zip = models.CharField(max_length=9)
+    one_time_pick_up = models.DateField("Date", default=datetime.date)
+    suspend_start_date = models.DateField("Date", default=datetime.date)
+    suspend_end_date = models.DateField("Date", default=datetime.date)
+    current_bill = models.FloatField(null=True, blank=True, default=None)
+    pick_up_charge_amount = models.FloatField(null=True, blank=True, default=None)
+    pick_up_zip = models.CharField(max_length=9, default=0)
 
     def __str__(self):
         return self.name
