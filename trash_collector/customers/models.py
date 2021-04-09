6528fbc2d99784082df8374941ed17e6115bdc9c
unknown <ssgsj.carranza@gmail.com>
2021-04-09 13:30:34 -0500

    populated customewr with day options for trash pick up

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -10,12 +10,12 @@ class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', default=1, on_delete=models.CASCADE)
     pickup_date = models.CharField(max_length=50)
-    one_time_pick_up = models.DateField()
-    suspend_start_date = models.DateField()
-    suspend_end_date = models.DateField()
-    current_bill = models.FloatField()
-    pick_up_charge_amount = models.FloatField()
-    pick_up_zip = models.CharField(max_length=9)
+    # one_time_pick_up = models.DateField()
+    # suspend_start_date = models.DateField()
+    # suspend_end_date = models.DateField()
+    # current_bill = models.FloatField()
+    # pick_up_charge_amount = models.FloatField()
+    # pick_up_zip = models.CharField(max_length=9)
 
     def __str__(self):
         return self.name
