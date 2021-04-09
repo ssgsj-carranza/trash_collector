0fda1a79054e7ff3c2d7b60c2d185703beac8542
unknown <ssgsj.carranza@gmail.com>
2021-04-09 12:58:30 -0500

    added an employee class and assigned name, user, and their route zip code

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
+    route_zipcode = models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.name
