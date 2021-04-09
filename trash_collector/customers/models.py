5dc248ff6b811e2443207169a5e5f999391cb5d4
unknown <ssgsj.carranza@gmail.com>
2021-04-09 12:54:25 -0500

    added a bill model along with zip, one time extra pick up, and suspend bill. Still needs testing

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -8,13 +8,22 @@ from django.db import models
 
 class Customer(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', default=0, on_delete=models.CASCADE)
-    pick_up_date = models.DateTimeField('date of pick up')
+    user = models.ForeignKey('accounts.User', default=1, on_delete=models.CASCADE)
+    pickup_date = models.CharField(max_length=50)
+    one_time_pick_up = models.DateField()
+    suspend_start_date = models.DateField()
+    suspend_end_date = models.DateField()
+    current_bill = models.FloatField()
+    pick_up_charge_amount = models.FloatField()
+    pick_up_zip = models.CharField(max_length=9)
 
     def __str__(self):
         return self.name
 
 
 class ChoiceDays(models.Model):
-    date = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.ForeignKey(Customer, default=1, on_delete=models.CASCADE)
     pick_up_choice = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.pick_up_choice
