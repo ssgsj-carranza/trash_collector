50635e7374e1a307a34e4a5994ed0c9c0fdecf18
unknown <ssgsj.carranza@gmail.com>
2021-04-13 14:23:22 -0500

    fully fixed beyond level error from my import

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -7,6 +7,7 @@ import datetime
 # from ..customers.models import Customer
 from os import path
 
+
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
@@ -14,7 +15,7 @@ from os import path
 
 def index(request):
     # Get the Customer model from the other app, it can now be used to query the db
-    customer = apps.get_model('customers.Customer')
+    Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
 
 
@@ -33,16 +34,19 @@ def create(request):
 
 
 def customer_in_zip(request):
-    customer: object = apps.get_model('customer.Customer')
+    customer: object = apps.get_model('customers.Customer')
     customers = customer.objects.all()
     in_zip = []
     for customer in customers:
         if customer.route_zipcode == request.POST.get('route_zipcode'):
             in_zip.append(customer)
     return HttpResponseRedirect(reverse('employees:index'))
+
+
 # customers_in_zip = customers.filter(address__zip_code__contains = employee.zip_code)
 
 def today_pick_up():
+    Customer = apps.get_model('customers.Customer')
     today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
     today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
     today_customer = Customer.objects.get(date__range=(today_min, today_max))
@@ -50,11 +54,11 @@ def today_pick_up():
 
 
 def extra_today_pick_up():
+    Customer = apps.get_model('customers.Customer')
     today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
     today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
     extra_customer = Customer.objects.get(date__range=(today_min, today_max))
     return extra_customer
-
 
 # https://realpython.com/transaction-management-with-django-1-6/
 # def non_suspended_account():
@@ -62,5 +66,3 @@ def extra_today_pick_up():
 #     non_suspended = []
 #     for customer in customers:
 #         if Customer.suspend_start_date and Customer.suspend_end_date
-
-
